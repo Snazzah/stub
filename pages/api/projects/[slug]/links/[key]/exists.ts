b57af6e6d59175fafab35cf35001f7bc1069d8a5
Snazzah <me@snazzah.com>
2022-09-26 19:39:49 -0500

    fix: fix link exists endpoint

--- pages/api/projects/[slug]/links/[key]/exists.ts
+++ pages/api/projects/[slug]/links/[key]/exists.ts
@@ -3,10 +3,10 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { withProjectAuth } from '@/lib/auth';
 import { checkIfKeyExists } from '@/lib/redis';
 
-export default withProjectAuth(async (req: NextApiRequest, res: NextApiResponse) => {
+export default withProjectAuth(async (req: NextApiRequest, res: NextApiResponse, project) => {
   if (req.method === 'GET') {
-    const { domain, key } = req.query as { domain: string; key: string };
-    const response = await checkIfKeyExists(domain, key);
+    const { key } = req.query as { key: string };
+    const response = await checkIfKeyExists(project.domain, key);
     return res.status(200).json(response);
   } else {
     res.setHeader('Allow', ['GET']);
