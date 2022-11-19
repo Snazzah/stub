7858ebfc434e5b5bd9c010f7a78355804f78be4e
Snazzah <me@snazzah.com>
2022-11-18 22:48:33 -0600

    chore: linting fix

--- components/app/modals/archive-link-modal.tsx
+++ components/app/modals/archive-link-modal.tsx
@@ -36,13 +36,7 @@ function ArchiveLinkModal({
     <Modal showModal={showArchiveLinkModal} setShowModal={setShowArchiveLinkModal}>
       <div className="inline-block w-full sm:max-w-md overflow-hidden align-middle transition-all transform bg-white sm:border sm:border-gray-200 shadow-xl sm:rounded-2xl">
         <div className="flex flex-col justify-center items-center space-y-3 sm:px-16 px-4 pt-8 py-4 border-b border-gray-200">
-          <BlurImage
-            src={getFaviconFromDomain(apexDomain)}
-            alt={apexDomain}
-            className="w-10 h-10 rounded-full"
-            width={20}
-            height={20}
-          />
+          <BlurImage src={getFaviconFromDomain(apexDomain)} alt={apexDomain} className="w-10 h-10 rounded-full" width={20} height={20} />
           <h3 className="font-medium text-lg">Archive {shortlink}</h3>
           <p className="text-sm text-gray-500 text-center">Archived links will still work - they just won't show up on your main dashboard.</p>
         </div>
